d2654dfabaad8c2e28c8bf51bb6d1f4e1dd3fea0
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-05-25 21:40:21 -0400

    adding middleware for logging to reducer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import user from './store/reducer'
-import {createStore} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(user);
+const logger = store => {
+    
+    return next => {
+        
+        return action => {
+            console.log('MIDDLEWARE current state',store.getState());
+            console.log('[MIDDLEWARE] dispatching action',action);
+            const result = next(action);
+            console.log('MIDDLEWARE next state',store.getState());
+            return result;
+        }
+    }
+}
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__  || compose;
+const store = createStore(user,composeEnhancer(applyMiddleware(logger,thunk)));
 
 ReactDOM.render(
     <React.StrictMode>
