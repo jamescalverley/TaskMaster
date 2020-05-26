64ace451c78d3be51f7b5fb062b3fc439a6a5350
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-05-25 21:39:17 -0400

    updating initial user state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,72 @@
-import * as actionType from './types';
+import * as actionType from "./types";
 const initialState = {
-    
-        user : {
-        email: '',
-        name: '',
-        firstname: '',
-        lastname: '',
-        dashboards: [{ columns: [], shared: [] }],
-        }
-    
-}
+  user: {
+    email: "",
+    name: "",
+    firstname: "",
+    lastname: "",
+    password: "",
+    user_settings: {
+      theme: "light",
+      profilePicUrl: "url",
+    },
+    dashboards: [
+      {
+        name: "Dashboard 1",
+        id: 0,
+        owner: "",
+        shared: [],
+        columns: [],
+        shared: [],
+      },
+    ],
+    sharedToUser: [],
+    sharedByUser: [],
+  },
+  currentDashboard: 0,
+};
 
-const user = (state=initialState,action) => {
-    switch (action.type) {
-        case actionType.ADD_COLUMN:
-            console.log('add column redux action triggered')
-            return state;
-        default:
-            return state;
-    }
-}
+const user = (state = initialState, action) => {
+  let newState = { ...state };
+  let currentDashboard = state.currentDashboard;
+  let dashboard;
+  switch (action.type) {
+    case actionType.ADD_COLUMN:
+      dashboard = { ...state.user.dashboards[currentDashboard] };
+      dashboard.columns.push(action.payload.column);
+      newState.user.dashboards[currentDashboard] = dashboard;
+      return newState;
 
+    case actionType.DELETE_COLUMN:
+      dashboard = { ...state.user.dashboards[currentDashboard] };
+      dashboard.columns.splice(action.payload.colIndex, 1);
+      newState.user.dashboards[currentDashboard] = dashboard;
+      return newState;
+
+    case actionType.SET_USER:
+      const oldState = { ...state };
+      newState = { ...oldState, ...action.payload };
+      return newState;
+
+    case actionType.DELETE_CARD:
+      return state;
+
+    case actionType.SET_CURR_DASHBOARD:
+      newState.currentDashboard = action.payload.dashboard;
+      return newState;
+
+    case actionType.UPDATE_COL_TITLE:
+      newState.user.dashboards[currentDashboard].columns[
+        action.payload.colIndex
+      ].name = action.payload.colTitle;
+      return newState;
+
+    case actionType.UPDATE_USER:
+      return state;
+
+    default:
+      return state;
+  }
+};
 
 export default user;
