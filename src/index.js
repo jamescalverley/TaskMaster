cd54c6c4d565268fe1acf8c5bbbf99c73ec69ee5
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-05-27 21:59:07 -0400

    removing logger function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import user from './store/reducer'
-import {createStore,applyMiddleware,compose} from 'redux';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
+import {createStore,applyMiddleware,compose} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
-const logger = store => {
-    
-    return next => {
-        
-        return action => {
-            console.log('MIDDLEWARE current state',store.getState());
-            console.log('[MIDDLEWARE] dispatching action',action);
-            const result = next(action);
-            console.log('MIDDLEWARE next state',store.getState());
-            return result;
-        }
-    }
-}
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__  || compose;
-const store = createStore(user,applyMiddleware(logger,thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(user,composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <React.StrictMode>
